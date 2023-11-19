0296756eec5ebaeb7e910336eddad2b512e32a80
techrishikb <68335914+techrishikb@users.noreply.github.com>
2023-11-19 17:32:16 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -41,7 +41,7 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 def get_conversation_chain(vectorstore):
-    #llm = ChatOpenAI()
+    #llm = ChatOpenAI(temperature=0)
     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
